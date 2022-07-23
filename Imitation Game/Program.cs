a78f0b2727d3b07798d75cb2e84359fefdd5e0b8
hasankr <hasankr@abv.bg>
2022-07-23 23:18:55 +0300

    added 1 and examPrep1

---
+++ Imitation Game/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Decode")
+                {
+                    break;
+                }
+                string[] tokens = command.Split('|');
+                string action = tokens[0];
+                switch (action)
+                {
+                    case "Move":
+                        int length = int.Parse(tokens[1]);
+                        string stringToMove = message.Substring(0, length);
+                        message = message.Remove(0, length);
+                        message += stringToMove;
+                        break;
+                    case "Insert":
+                        int index = int.Parse(tokens[1]);
+                        string value = tokens[2];
+                        message = message.Insert(index, value);
+                        break;
+                    case "ChangeAll":
+                        string substring = tokens[1];
+                        string replacement = tokens[2];
+                        message = message.Replace(substring, replacement);
+                        break;
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
